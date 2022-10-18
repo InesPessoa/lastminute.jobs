49d3c306ec1347529a1e55310cd7e8295f024605
Ines <inesmspessoa@gmail.com>
2022-10-18 16:59:46 +0100

    authentication endpoints

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,54 +1,45 @@
 const User = require("./../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("./../utils/appError");
 
-exports.getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json({
-      status: "sucess",
-      users,
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: "error",
-      error: err,
-      message: "Something went wrong",
-    });
+const filterObj = (obj, ...allowerdFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowerdFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // create error if the user posts password data
+  if (req.body.password || req.body.passwordConfig) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use updateMyPassword",
+        400
+      )
+    );
   }
-};
+  // filter out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, "name", "email");
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
+  //update user document
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidatoes: true,
   });
-};
-
-exports.createUser = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(200).json({
-      status: "sucess",
-      user,
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: "error",
-      error: err,
-      message: "Something went wrong",
-    });
-  }
-};
-
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
+  res.status(200).json({
+    status: "sucess",
+    data: { user: updateUser },
   });
-};
+});
 
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(204).json({
+    status: "sucess",
+    data: null,
   });
-};
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,19 @@
 const express = require("express");
+const authController = require("./../controllers/authController");
 const userController = require("./../controllers/userController");
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
-
-router
-  .route("/:id")
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+router.post("/singup", authController.signup);
+router.post("/login", authController.login);
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
+router.patch(
+  "/updateMyPassword",
+  authController.protect,
+  authController.updatePassword
+);
+router.patch("/updateMe", authController.protect, userController.updateMe);
+router.delete("/deleteMe", authController.protect, userController.deleteMe);
 
 module.exports = router;
