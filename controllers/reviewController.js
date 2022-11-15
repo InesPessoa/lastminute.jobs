dd743e9ec5bdfa3136b57b51327116daf7f5f759
Ines Pessoa <inespessoa@mbp-de-ines.lan>
2022-11-15 17:01:46 +0000

    cleanning controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,23 +4,15 @@ const Job = require("./../models/jobModel");
 const Application = require("./../models/applicationModel");
 const Review = require("./../models/reviewModel");
 
-//Todo create review
-
-//get review by id
-
-//get reviews
-
 exports.createReview = catchAsync(async (req, res, next) => {
   const userId = req.user.id;
   let jobId;
   try {
     jobId = req.body.jobId;
-    console.log(jobId);
   } catch {
     return next(new AppError("Please send the application Id", 400));
   }
   const job = await Job.findById(jobId);
-  console.log(job._id);
   if (job.executionDate > Date.now()) {
     return next(new AppError("The job didn't happen yet", 400));
   }
@@ -30,7 +22,6 @@ exports.createReview = catchAsync(async (req, res, next) => {
       return next(new AppError("The user is not an employee on the job", 400));
     }
   } else if (userRole == "employer") {
-    console.log(job.employerId._id);
     if (job.employerId._id != userId) {
       return next(new AppError("The user is not an employer on the job", 400));
     }
