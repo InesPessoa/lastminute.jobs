9f17bdaefc8b6612646a4e724d3a7ff3ef07e57f
Ines Pessoa <inespessoa@mbp-de-ines.lan>
2022-11-15 17:40:53 +0000

    add security to the application

--- app.js
+++ app.js
@@ -44,6 +44,33 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+//limit requests from the same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP. please try again in an hour!",
+});
+
+app.use("/api", limiter);
+
+//body parser, reading data from the body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+//data sanitization against NoSql query injection
+app.use(mongoSanitize());
+
+//data sanitization agains XSS
+app.use(xss());
+
+//prevent parametar polution
+app.use(
+  hpp({
+    whitelist: [
+      // "example",
+    ],
+  })
+);
+
 app.use(express.json());
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
